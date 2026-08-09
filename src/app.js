6d90548776c5c99ef1a5f4e1e3cbaba4950b71c2
mohamed.noui <mohamed.noui@emploitic.com>
2026-08-09 08:26:34 +0100

    implemet users crud and dockerization

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
+import usersRoutes from '#routes/users.routes.js';
 
 const app = express();
 
@@ -24,6 +26,21 @@ app.get('/', (req, res) => {
 
   res.status(200).send('Hello from Devops!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Devops API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
