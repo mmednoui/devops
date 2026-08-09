7d0f724cc1e116058608d57d71d96171071d7e46
mohamed.noui <mohamed.noui@emploitic.com>
2026-08-09 05:42:30 +0100

    implemet eslint and pretiter

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from Devops!");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Devops!');
 });
 
 app.use((req, res) => {
-  res.status(404).json({ error: "Route not found" });
+  res.status(404).json({ error: 'Route not found' });
 });
 
 export default app;
